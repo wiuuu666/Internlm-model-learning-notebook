3abb5ea185bd2a2fb3ac737980e932c60abaa1e5
wiuuu666 <116445041+wiuuu666@users.noreply.github.com>
2024-05-11 13:01:23 +0800

    app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 
 # download internlm2 to the base_path directory using git tool
-base_path = '.wiuuu/niuma-self-model'
-os.system(f'git clone git@code.openxlab.org.cn:wiuuu/niuma-self-model.git {base_path}')
+base_path = '.niuma-self-model'
+os.system(f'git clone https://code.openxlab.org.cn/wiuuu/niuma-self-model.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
 tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
